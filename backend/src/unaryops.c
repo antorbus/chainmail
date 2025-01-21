c60fd0db6e2d65db89069704ae385bb2fe0c72c5
ai7ana <aitanaviudes@gmail.com>
2025-01-21 10:52:57 +0100

    add unary kernels

--- backend/src/unaryops.c
+++ backend/src/unaryops.c
@@ -20,7 +20,6 @@ BACKWARD_FUNC_DEF(u_op_exp_backward){
     return seed;
 }
 
-
 FORWARD_FUNC_DEF(u_op_pow_forward){
     lemur_float x = k1->array[0];
     KERNEL_TENSOR_5D_LOOP_START(kr){
@@ -79,6 +78,124 @@ BACKWARD_FUNC_DEF(u_op_sigmoid_backward){
         size_t offset_kr = KERNEL_TENSOR_GET_OFFSET(kr);
         lemur_float sigmoid_val = kr->array[offset_kr];
         seed->array[offset_seed] *= sigmoid_val * (1.0 - sigmoid_val);
+    }
+    return seed;
+}
+
+FORWARD_FUNC_DEF(u_op_log_forward){
+    (void) k1;
+    KERNEL_TENSOR_5D_LOOP_START(kr){
+        size_t offset_k0 = KERNEL_TENSOR_GET_OFFSET(k0);
+        size_t offset_kr = KERNEL_TENSOR_GET_OFFSET(kr);
+        kr->array[offset_kr] = logf(k0->array[offset_k0]);
+    }
+}
+
+BACKWARD_FUNC_DEF(u_op_log_backward) {
+    (void) idx; (void) k1; (void) kr;
+    kernel_tensor *reciprocal_result = empty_kernel_tensor_like(seed);
+    u_op_reciprocal_forward(reciprocal_result, k0, NULL);
+    KERNEL_TENSOR_5D_LOOP_START(seed) {
+        size_t offset_seed = KERNEL_TENSOR_GET_OFFSET(seed);
+        size_t offset_reciprocal = KERNEL_TENSOR_GET_OFFSET(reciprocal_result);
+        seed->array[offset_seed] *= reciprocal_result->array[offset_reciprocal];
+    }
+    free_kernel_tensor(reciprocal_result);
+    return seed;
+}
+
+FORWARD_FUNC_DEF(u_op_neg_forward){
+    (void) k1;
+    KERNEL_TENSOR_5D_LOOP_START(kr){
+        size_t offset_k0 = KERNEL_TENSOR_GET_OFFSET(k0);
+        size_t offset_kr = KERNEL_TENSOR_GET_OFFSET(kr);
+        kr->array[offset_kr] = -1.0 * k0->array[offset_k0];
+    }
+}
+
+BACKWARD_FUNC_DEF(u_op_neg_backward){
+    (void) k1; (void) k0; (void) kr; (void) idx;
+    KERNEL_TENSOR_5D_LOOP_START(seed){
+        size_t offset_seed = KERNEL_TENSOR_GET_OFFSET(seed);
+        seed->array[offset_seed] *= -1.0;
+    }
+    return seed;
+}
+
+FORWARD_FUNC_DEF(u_op_sqrt_forward){
+    (void) k1;
+    KERNEL_TENSOR_5D_LOOP_START(kr){
+        size_t offset_k0 = KERNEL_TENSOR_GET_OFFSET(k0);
+        size_t offset_kr = KERNEL_TENSOR_GET_OFFSET(kr);
+        kr->array[offset_kr] = sqrt(k0->array[offset_k0]);
+    }
+}
+
+BACKWARD_FUNC_DEF(u_op_sqrt_backward){
+    (void) k1; (void) k0; (void) kr; (void) idx;
+    KERNEL_TENSOR_5D_LOOP_START(seed){
+        size_t offset_seed = KERNEL_TENSOR_GET_OFFSET(seed);
+        size_t offset_kr = KERNEL_TENSOR_GET_OFFSET(kr);
+        lemur_float sqrt_val = kr->array[offset_kr];
+        seed->array[offset_seed] *= 1.0 / (2 * sqrt_val);
+    }
+    return seed;
+}
+
+FORWARD_FUNC_DEF(u_op_abs_forward){
+    (void) k1;
+    KERNEL_TENSOR_5D_LOOP_START(kr){
+        size_t offset_k0 = KERNEL_TENSOR_GET_OFFSET(k0);
+        size_t offset_kr = KERNEL_TENSOR_GET_OFFSET(kr);
+        kr->array[offset_kr] = fabs(k0->array[offset_k0]);
+    }
+}
+
+BACKWARD_FUNC_DEF(u_op_abs_backward){
+    (void) k1; (void) idx;
+    KERNEL_TENSOR_5D_LOOP_START(seed){
+        size_t offset_seed = KERNEL_TENSOR_GET_OFFSET(seed);
+        size_t offset_k0 = KERNEL_TENSOR_GET_OFFSET(k0);
+        size_t offset_kr = KERNEL_TENSOR_GET_OFFSET(kr);
+        seed->array[offset_seed] *= k0->array[offset_k0] / kr->array[offset_kr];;
+    }
+    return seed;
+}
+
+FORWARD_FUNC_DEF(u_op_sign_forward){
+    (void) k1;
+    KERNEL_TENSOR_5D_LOOP_START(kr){
+        size_t offset_k0 = KERNEL_TENSOR_GET_OFFSET(k0);
+        size_t offset_kr = KERNEL_TENSOR_GET_OFFSET(kr);
+        lemur_float v = k0->array[offset_k0];
+        kr->array[offset_kr] = (v > 0) - (v < 0);
+    }
+}
+
+BACKWARD_FUNC_DEF(u_op_sign_backward) {
+    (void) k1; (void) k0; (void) kr; (void) idx;
+    KERNEL_TENSOR_5D_LOOP_START(seed){
+        size_t offset_seed = KERNEL_TENSOR_GET_OFFSET(seed);
+        seed->array[offset_seed] *= 0;
+    }
+    return seed;
+}
+
+FORWARD_FUNC_DEF(u_op_reciprocal_forward){
+    (void) k1;
+    KERNEL_TENSOR_5D_LOOP_START(kr){
+        size_t offset_k0 = KERNEL_TENSOR_GET_OFFSET(k0);
+        size_t offset_kr = KERNEL_TENSOR_GET_OFFSET(kr);
+        kr->array[offset_kr] = 1.0 / k0->array[offset_k0];
+    }
+}
+
+BACKWARD_FUNC_DEF(u_op_reciprocal_backward){
+    (void) k1; (void) k0; (void) idx;
+    KERNEL_TENSOR_5D_LOOP_START(seed){
+        size_t offset_seed = KERNEL_TENSOR_GET_OFFSET(seed);
+        size_t offset_kr = KERNEL_TENSOR_GET_OFFSET(kr);
+        seed->array[offset_seed] *= -1.0 * (kr->array[offset_kr] * kr->array[offset_kr]);
     }
     return seed;
 }
