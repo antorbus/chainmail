d93d7639c87c71a1477f6564a2273b43c0446c42
Andres Torrubia <antorwolf11@gmail.com>
2025-02-01 15:13:34 -0500

    finished expand kernel

--- backend/src/kernels/shapeops.c
+++ backend/src/kernels/shapeops.c
@@ -23,14 +23,43 @@ BACKWARD_FUNC_DEF(s_op_view_backward){
 }
 
 FORWARD_FUNC_DEF(s_op_expand_forward){
-    (void) kr;  (void) k0;  (void) k1;
-
+    (void) k0;  
+    for (size_t i = 0; i < 5; i++){
+        if ((kr->shape[i] == 1) && (1 != (size_t) k1->array[i])){
+            kr->stride[i] = 0;
+        }
+        kr->shape[i] = (size_t) k1->array[i];
+    } 
 }
 
 //expand (the dimension that was expanded get reduced with a sum!) so next_seed has same shape
 BACKWARD_FUNC_DEF(s_op_expand_backward){
-    (void) kr;  (void) k0;  (void) k1;  (void) seed;   (void) idx;
-    return NULL;
+    (void) k1; (void) idx;
+    
+    lemur_float dim_arr[5];
+    kernel_tensor dims;
+    dims.array = dim_arr;
+    dims.length = 5;
+    dims.shallow = false;
+
+    dims.shape[0] = 1;
+    dims.shape[1] = 1;
+    dims.shape[2] = 1;
+    dims.shape[3] = 1;
+    dims.shape[4] = 5;
+
+    set_contiguous_stride(&dims);
+
+    for (size_t i = 0; i < 5; i++){
+        if (k0->shape[i] == kr->shape[i]){
+            dim_arr[i] = 1;
+        } else {
+            dim_arr[i] = 0;
+        }
+    } 
+    kernel_tensor *next_seed = empty_contiguous_kernel_tensor(k0->shape);
+    forward_func_table[OP_SUM](next_seed, seed, &dims);
+    return next_seed;
 }
 
 //permute literally permutes strides
