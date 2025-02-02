779b548b2121a18ef57312d1b01af9c6da752ec2
Andres Torrubia <antorwolf11@gmail.com>
2025-02-02 01:55:17 -0500

    found bug: reduce ops with openmp lead to race condition because of KERNEL_TENSOR_5D_LOOP_START

--- tests/tests.c
+++ tests/tests.c
@@ -25,12 +25,12 @@ int test_basic_add_mul(){
 
     backward(v); 
     
-    if (v->grad->array[0] != 1.0) errorval+= 1;
-    if (w->grad->array[0] != 1.0) errorval+= 2;
-    if (z->grad->array[0] != 1.0) errorval+= 4;
-    if (x->grad->array[0] != 6.0) errorval+= 8;
-    if (y->grad->array[0] != 1.0) errorval+= 16;
-    if (v->k->array[0] != 5.0) errorval+= 32;
+    if (v->grad->array[0] != 1.0) errorval+= 1<<0;
+    if (w->grad->array[0] != 1.0) errorval+= 1<<1;
+    if (z->grad->array[0] != 1.0) errorval+= 1<<2;
+    if (x->grad->array[0] != 6.0) errorval+= 1<<3;
+    if (y->grad->array[0] != 1.0) errorval+= 1<<4;
+    if (v->k->array[0] != 5.0) errorval+= 1<<5;
 
     free_tensor(x);
     free_tensor(y);
@@ -66,8 +66,59 @@ int test_view(){
     return errorval;
 }
 
-int test_expand(){
-    int errorval = -1;
+int test_expand_sum(){
+    int errorval = 0;
+
+    size_t shape[5] = {1,1,1,1,4};
+    size_t shape_dim[5] = {1,1,1,1,5};
+    tensor *dim_e = empty_tensor(shape_dim, false);
+    dim_e->k->array[0] = 2.0;
+    dim_e->k->array[1] = 1.0;
+    dim_e->k->array[2] = 1.0;
+    dim_e->k->array[3] = 2.0;
+    dim_e->k->array[4] = 4.0;
+    tensor *dim_s = empty_tensor(shape_dim, false);
+    dim_s->k->array[0] = 0.0;
+    dim_s->k->array[1] = 0.0;
+    dim_s->k->array[2] = 0.0;
+    dim_s->k->array[3] = 0.0;
+    dim_s->k->array[4] = 0.0;
+
+    tensor *a = empty_tensor(shape, true);
+    tensor *b = empty_tensor(shape, true);
+    linspace_kernel_tensor(a->k, 0.0, 4.0-1.0);
+    linspace_kernel_tensor(b->k, -4.0, 0.0-1.0);
+    tensor *c = mul(a, b, false);
+    tensor *d = expand(c, dim_e, false);
+    tensor *e = sum(d, dim_s, false);
+
+    int num_backward = 100;
+    for (int i =0; i < num_backward; i++){
+        backward(e);
+    }
+   
+    if (a->grad->array[0] != -16.0 * num_backward) errorval+= 1<<0;
+    if (a->grad->array[1] != -12.0 * num_backward) errorval+= 1<<1;
+    if (a->grad->array[2] != -8.0 * num_backward) errorval+= 1<<2;
+    if (a->grad->array[3] != -4.0 * num_backward) errorval+= 1<<3;
+    if (b->grad->array[0] != 0.0 * num_backward) errorval+= 1<<4;
+    if (b->grad->array[1] != 4.0 * num_backward) errorval+= 1<<5;
+    if (b->grad->array[2] != 8.0 * num_backward) errorval+= 1<<6;
+    if (b->grad->array[3] != 12.0 * num_backward) errorval+= 1<<7;
+
+    if (errorval != 0){
+        print_tensor(a);
+        print_tensor(b);
+    }
+
+    free_tensor(a);
+    free_tensor(b);
+    free_tensor(c);
+    free_tensor(d);
+    free_tensor(e);
+    free_tensor(dim_e);
+    free_tensor(dim_s);
+
     return errorval;
 }
 
@@ -83,7 +134,7 @@ test_func tests[] = {
     test_sum,
     test_sigmoid,
     test_view,
-    test_expand,
+    test_expand_sum,
     test_permute,
 
 };
