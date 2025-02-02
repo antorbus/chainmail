9430f1af562bcd90cdb5e6e3621176df3ba1603f
Andres Torrubia <antorwolf11@gmail.com>
2025-02-01 19:18:08 -0500

    fixed mm compile warnings

--- backend/src/kernels/matmul.c
+++ backend/src/kernels/matmul.c
@@ -2,19 +2,20 @@
 
 
 FORWARD_FUNC_DEF(m_op_mm_forward){
-
+    (void) kr; (void) k0; (void) k1;
 }
 
 BACKWARD_FUNC_DEF(m_op_mm_backward){
-
+    (void) kr; (void) k0; (void) k1; (void) seed; (void) idx;
     return NULL;
 }
 
 FORWARD_FUNC_DEF(m_op_bmm_forward){
+    (void) kr; (void) k0; (void) k1;
 
 }
 
 BACKWARD_FUNC_DEF(m_op_bmm_backward){
-
+    (void) kr; (void) k0; (void) k1; (void) seed; (void) idx;
     return NULL;
 }
