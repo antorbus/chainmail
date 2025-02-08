745fd4faae175401beea30283f769df27c792576
Andres Torrubia <antorwolf11@gmail.com>
2025-02-07 23:45:53 -0500

    added first python test

--- tests/pytests.py
+++ tests/pytests.py
@@ -1,19 +1,16 @@
-import sys
 import unittest
-sys.path.append('../')
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import lemur
 
 class TestLightLemur(unittest.TestCase):
     
     def test_basic(self):
-        self.assertEqual(1, 1, "Grad check for v failed")
-
-    def test_basic2(self):
-        self.assertEqual(1, 1, "Grad check for v failed")
-
-    def test_basic3(self):
-        self.assertEqual(1, 1, "Grad check for v failed")
-
+        a = lemur.full((1,1,1,1,10), 15, requires_grad=True)
+        (a + a).sum().backward()
+        real_grad = lemur.full((1,1,1,1,10), 2)
+        self.assertTrue((real_grad == a.grad).all(), "Grad check failed")
 
     def test_add(self):
         self.skipTest("Not implemented")
